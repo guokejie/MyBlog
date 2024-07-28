d4439dd37e6cc61d49343be78e73943fa417e7bb
guokejie <kejieguocs@163.com>
2024-07-29 00:08:01 +0800

    初始化redis数据

--- sangeng-blog/src/main/java/com/sangeng/SanGengBlogApplication.java
+++ sangeng-blog/src/main/java/com/sangeng/SanGengBlogApplication.java
@@ -3,9 +3,11 @@ package com.sangeng;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @MapperScan("com.sangeng.mapper")
+@EnableScheduling // 启动定时任务
 public class SanGengBlogApplication {
     public static void main(String[] args) {
         SpringApplication.run(SanGengBlogApplication.class, args);
